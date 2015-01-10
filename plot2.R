368e5aa25f0cdd7f2152e0fa299d90582ba7be6e
Michael Robichaud <robi@Michaels-MacBook-Air.local>
2015-01-10 14:53:46 -0800

    make code more readable

--- plot2.R
+++ plot2.R
@@ -1,6 +1,11 @@
 
 # Read the raw data
-raw <- read.table('household_power_consumption.txt', sep=";", header=TRUE, na.strings=c("","?","NA"), colClasses=c("character", "character", rep("numeric",7)))
+raw <- read.table('household_power_consumption.txt',
+				  sep=";",
+				  header=TRUE,
+				  na.strings=c("","?","NA"),
+				  colClasses=c("character", "character",
+				  rep("numeric",7)))
 
 # Get the subset of data with date range 1/2/2007 - 2/2/2007
 data <- subset(raw, raw$Date == "1/2/2007" | raw$Date == "2/2/2007")
@@ -12,7 +17,10 @@ data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:
 png(file="plot2.png",width=480,height=480)
 
 # Plot the graph
-plot(data$Global_active_power~data$DateTime, type="l", xlab=NA, ylab="Global Active Power (kilowatts)")
+plot(data$Global_active_power~data$DateTime,
+	 type="l",
+	 xlab=NA,
+	 ylab="Global Active Power (kilowatts)")
   
 # Finish the png
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,6 +1,11 @@
 
 # Read the raw data
-raw <- read.table('household_power_consumption.txt', sep=";", header=TRUE, na.strings=c("","?","NA"), colClasses=c("character", "character", rep("numeric",7)))
+raw <- read.table('household_power_consumption.txt',
+				  sep=";",
+				  header=TRUE,
+				  na.strings=c("","?","NA"),
+				  colClasses=c("character", "character",
+				  rep("numeric",7)))
 
 # Get the subset of data with date range 1/2/2007 - 2/2/2007
 data <- subset(raw, raw$Date == "1/2/2007" | raw$Date == "2/2/2007")
@@ -12,10 +17,17 @@ data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:
 png(file="plot3.png",width=480,height=480)
 
 # Plot the graph
-plot(data$Sub_metering_1~data$DateTime, col="black", type="l", xlab=NA, ylab="Energy sub metering")
+plot(data$Sub_metering_1~data$DateTime,
+	 col="black",
+	 type="l",
+	 xlab=NA,
+	 ylab="Energy sub metering")
 lines(data$Sub_metering_2~data$DateTime, col="red", type="l")
 lines(data$Sub_metering_3~data$DateTime, col="blue", type="l")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1,col=c("black", "red", "blue"))
+legend("topright",
+	   legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+	   lwd=1,
+	   col=c("black", "red", "blue"))
   
 # Finish the png
 dev.off()
